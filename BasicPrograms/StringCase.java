0efbf9dac74fff05384dbbdf79de9780826e38f1
suman <sumanbhamu@rocketmail.com>
2017-02-09 09:36:04 -0800

    Update StringCase.java

--- BasicPrograms/StringCase.java
+++ BasicPrograms/StringCase.java
@@ -1,4 +1,8 @@
 import java.util.Scanner;
+/*2.You have been given a String S consisting of uppercase and lowercase English alphabets.
+You need to change the case of each alphabet in this String.
+That is, all the uppercase letters should be converted to lowercase and all the lowercase 
+letters should be converted to uppercase. You need to then print the resultant String to output.*/
 
 public class StringCase {
 	
